829a374640732cc89c73bd74df256aeada8fcf31
Odis Clemons <odis.clemons@gmail.com>
2022-06-04 17:57:43 -0400

    added invalid tags to test.js for testing

--- test.js
+++ test.js
@@ -4,6 +4,13 @@ let testStr = `
     <p>this is a sample of code and stuff.</p>
 </div>
 </>
+// invalid tags below shouldnt be returned:
+
+<invalid
+invalid>
+<12313>
+<></>
+<&**&*^&^>
 `;
 
 let regex = /<\/?[a-z]+>/gi;
